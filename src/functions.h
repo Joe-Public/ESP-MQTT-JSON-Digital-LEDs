01ee65a33419953f229588fa4e85d0b346e1956d
Johannes Krausmueller <johannes@krausmueller.de>
2018-10-10 21:30:39 +0200

    moved funtions to extra file

---
+++ src/functions.h
@@ -0,0 +1,475 @@
+/********************************** START SEND STATE*****************************************/
+void sendState() {
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+
+  root["state"] = (stateOn) ? on_cmd : off_cmd;
+  JsonObject& color = root.createNestedObject("color");
+  color["r"] = red;
+  color["g"] = green;
+  color["b"] = blue;
+
+  root["brightness"] = brightness;
+  root["effect"] = effectString.c_str();
+
+
+  char buffer[root.measureLength() + 1];
+  root.printTo(buffer, sizeof(buffer));
+
+  client.publish(MQTT_STATE_TOPIC, buffer, true);
+}
+
+
+/********************************** START Set Color*****************************************/
+void setColor(int inR, int inG, int inB) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].red   = inR;
+    leds[i].green = inG;
+    leds[i].blue  = inB;
+  }
+
+  FastLED.show();
+
+  Serial.println("Setting LEDs:");
+  Serial.print("r: ");
+  Serial.print(inR);
+  Serial.print(", g: ");
+  Serial.print(inG);
+  Serial.print(", b: ");
+  Serial.println(inB);
+}
+
+
+/**************************** START TRANSITION FADER *****************************************/
+// From https://www.arduino.cc/en/Tutorial/ColorCrossfader
+/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
+  The program works like this:
+  Imagine a crossfade that moves the red LED from 0-10,
+    the green from 0-5, and the blue from 10 to 7, in
+    ten steps.
+    We'd want to count the 10 steps and increase or
+    decrease color values in evenly stepped increments.
+    Imagine a + indicates raising a value by 1, and a -
+    equals lowering it. Our 10 step fade would look like:
+    1 2 3 4 5 6 7 8 9 10
+  R + + + + + + + + + +
+  G   +   +   +   +   +
+  B     -     -     -
+  The red rises from 0 to 10 in ten steps, the green from
+  0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
+  In the real program, the color percentages are converted to
+  0-255 values, and there are 1020 steps (255*4).
+  To figure out how big a step there should be between one up- or
+  down-tick of one of the LED values, we call calculateStep(),
+  which calculates the absolute gap between the start and end values,
+  and then divides that gap by 1020 to determine the size of the step
+  between adjustments in the value.
+*/
+int calculateStep(int prevValue, int endValue) {
+  int step = endValue - prevValue; // What's the overall gap?
+  if (step) {                      // If its non-zero,
+    step = 1020 / step;          //   divide by 1020
+  }
+
+  return step;
+}
+/* The next function is calculateVal. When the loop value, i,
+   reaches the step size appropriate for one of the
+   colors, it increases or decreases the value of that color by 1.
+   (R, G, and B are each calculated separately.)
+*/
+int calculateVal(int step, int val, int i) {
+  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+    if (step > 0) {              //   increment the value if step is positive...
+      val += 1;
+    }
+    else if (step < 0) {         //   ...or decrement it if step is negative
+      val -= 1;
+    }
+  }
+
+  // Defensive driving: make sure val stays in the range 0-255
+  if (val > 255) {
+    val = 255;
+  }
+  else if (val < 0) {
+    val = 0;
+  }
+
+  return val;
+}
+
+
+/**************************** START STRIPLED PALETTE *****************************************/
+void setupStripedPalette( CRGB A, CRGB AB, CRGB B, CRGB BA) {
+  currentPalettestriped = CRGBPalette16(
+                            A, A, A, A, A, A, A, A, B, B, B, B, B, B, B, B
+                            //    A, A, A, A, A, A, A, A, B, B, B, B, B, B, B, B
+                          );
+}
+
+
+/********************************** START FADE************************************************/
+void fadeall() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].nscale8(250);  //for CYCLon
+  }
+}
+
+
+/********************************** START FIRE **********************************************/
+void Fire2012WithPalette()
+{
+  // Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for ( int k = NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  }
+
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if ( random8() < SPARKING ) {
+    int y = random8(7);
+    heat[y] = qadd8( heat[y], random8(160, 255) );
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for ( int j = 0; j < NUM_LEDS; j++) {
+    // Scale the heat value from 0-255 down to 0-240
+    // for best results with color palettes.
+    byte colorindex = scale8( heat[j], 240);
+    CRGB color = ColorFromPalette( gPal, colorindex);
+    int pixelnumber;
+    if ( gReverseDirection ) {
+      pixelnumber = (NUM_LEDS - 1) - j;
+    } else {
+      pixelnumber = j;
+    }
+    leds[pixelnumber] = color;
+  }
+}
+
+
+/********************************** START ADD GLITTER *********************************************/
+void addGlitter( fract8 chanceOfGlitter)
+{
+  if ( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+
+/********************************** START ADD GLITTER COLOR ****************************************/
+void addGlitterColor( fract8 chanceOfGlitter, int red, int green, int blue)
+{
+  if ( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB(red, green, blue);
+  }
+}
+
+
+/********************************** START SHOW LEDS ***********************************************/
+void showleds() {
+
+  delay(1);
+
+  if (stateOn) {
+    FastLED.setBrightness(brightness);  //EXECUTE EFFECT COLOR
+    FastLED.show();
+    if (transitionTime > 0 && transitionTime < 130) {  //Sets animation speed based on receieved value
+      FastLED.delay(1000 / transitionTime);
+      //delay(10*transitionTime);
+    }
+  }
+  else if (startFade) {
+    setColor(0, 0, 0);
+    startFade = false;
+  }
+}
+void temp2rgb(unsigned int kelvin) {
+    int tmp_internal = kelvin / 100.0;
+    
+    // red 
+    if (tmp_internal <= 66) {
+        red = 255;
+    } else {
+        float tmp_red = 329.698727446 * pow(tmp_internal - 60, -0.1332047592);
+        if (tmp_red < 0) {
+            red = 0;
+        } else if (tmp_red > 255) {
+            red = 255;
+        } else {
+            red = tmp_red;
+        }
+    }
+    
+    // green
+    if (tmp_internal <=66){
+        float tmp_green = 99.4708025861 * log(tmp_internal) - 161.1195681661;
+        if (tmp_green < 0) {
+            green = 0;
+        } else if (tmp_green > 255) {
+            green = 255;
+        } else {
+            green = tmp_green;
+        }
+    } else {
+        float tmp_green = 288.1221695283 * pow(tmp_internal - 60, -0.0755148492);
+        if (tmp_green < 0) {
+            green = 0;
+        } else if (tmp_green > 255) {
+            green = 255;
+        } else {
+            green = tmp_green;
+        }
+    }
+    
+    // blue
+    if (tmp_internal >=66) {
+        blue = 255;
+    } else if (tmp_internal <= 19) {
+        blue = 0;
+    } else {
+        float tmp_blue = 138.5177312231 * log(tmp_internal - 10) - 305.0447927307;
+        if (tmp_blue < 0) {
+            blue = 0;
+        } else if (tmp_blue > 255) {
+            blue = 255;
+        } else {
+            blue = tmp_blue;
+        }
+    }
+}
+
+
+/********************************** START SETUP WIFI*****************************************/
+void setup_wifi() {
+  boolean wifiFound = false;
+  int i, n;
+
+  delay(10);
+
+  #ifdef SCAN_WIFI
+    Serial.println(F("scan start"));
+    int nbVisibleNetworks = WiFi.scanNetworks();
+    Serial.println(F("scan done"));
+    if (nbVisibleNetworks == 0) {
+      Serial.println(F("no networks found. Reset to try again"));
+      while (true); // no need to go further, hang in there, will auto launch the Soft WDT reset
+    }
+
+    Serial.print(nbVisibleNetworks);
+    Serial.println(" network(s) found");
+
+    for (i = 0; i < nbVisibleNetworks; ++i) {
+      Serial.println(WiFi.SSID(i)); // Print current SSID
+      for (n = 0; n < KNOWN_SSID_COUNT; n++) { // walk through the list of known SSID and check for a match
+        if (strcmp(KNOWN_SSID[n], WiFi.SSID(i).c_str())) {
+          Serial.print(F("\tNot matching "));
+          Serial.println(KNOWN_SSID[n]);
+        } else { // we got a match
+          wifiFound = true;
+          break; // n is the network index we found
+        }
+      } // end for each known wifi SSID
+      if (wifiFound) break; // break from the "for each visible network" loop
+    } // end for each visible network
+
+    if (!wifiFound) {
+      Serial.println(F("no Known network identified. Reset to try again"));
+      while (true); // no need to go further, hang in there, will auto launch the Soft WDT reset
+    }
+  #else
+    n = 0;
+  #endif
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(KNOWN_SSID[n]);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname(HOSTNAME);
+  WiFi.begin(KNOWN_SSID[n], KNOWN_PASSWORD[n]);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/*
+  SAMPLE PAYLOAD:
+  {
+    "brightness": 120,
+    "color": {
+      "r": 255,
+      "g": 100,
+      "b": 100
+    },
+    "flash": 2,
+    "transition": 5,
+    "state": "ON"
+  }
+*/
+
+
+/********************************** START PROCESS JSON*****************************************/
+bool processJson(char* message) {
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.parseObject(message);
+
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return false;
+  }
+
+  if (root.containsKey("state")) {
+    if (strcmp(root["state"], on_cmd) == 0) {
+      stateOn = true;
+    }
+    else if (strcmp(root["state"], off_cmd) == 0) {
+      stateOn = false;
+      onbeforeflash = false;
+    }
+  }
+
+  // If "flash" is included, treat RGB and brightness differently
+  if (root.containsKey("flash")) {
+    flashLength = (int)root["flash"] * 1000;
+
+    oldeffectString = effectString;
+
+    if (root.containsKey("brightness")) {
+      flashBrightness = root["brightness"];
+    }
+    else {
+      flashBrightness = brightness;
+    }
+
+    if (root.containsKey("color")) {
+      flashRed = root["color"]["r"];
+      flashGreen = root["color"]["g"];
+      flashBlue = root["color"]["b"];
+    }
+    else {
+      flashRed = red;
+      flashGreen = green;
+      flashBlue = blue;
+    }
+
+    if (root.containsKey("effect")) {
+      effect = root["effect"];
+      effectString = effect;
+      twinklecounter = 0; //manage twinklecounter
+    }
+
+    if (root.containsKey("transition")) {
+      transitionTime = root["transition"];
+    }
+    else if ( effectString == "solid") {
+      transitionTime = 0;
+    }
+
+    flashRed = map(flashRed, 0, 255, 0, flashBrightness);
+    flashGreen = map(flashGreen, 0, 255, 0, flashBrightness);
+    flashBlue = map(flashBlue, 0, 255, 0, flashBrightness);
+
+    flash = true;
+    startFlash = true;
+  }
+  else { // Not flashing
+    flash = false;
+
+    if (stateOn) {   //if the light is turned on and the light isn't flashing
+      onbeforeflash = true;
+    }
+
+    if (root.containsKey("color")) {
+      red = root["color"]["r"];
+      green = root["color"]["g"];
+      blue = root["color"]["b"];
+    }
+    
+    if (root.containsKey("color_temp")) {
+      //temp comes in as mireds, need to convert to kelvin then to RGB
+      int color_temp = root["color_temp"];
+      unsigned int kelvin  = 1000000 / color_temp;
+      
+      temp2rgb(kelvin);
+      
+    }
+
+    if (root.containsKey("brightness")) {
+      brightness = root["brightness"];
+    }
+
+    if (root.containsKey("effect")) {
+      effect = root["effect"];
+      effectString = effect;
+      twinklecounter = 0; //manage twinklecounter
+    }
+
+    if (root.containsKey("transition")) {
+      transitionTime = root["transition"];
+    }
+    else if ( effectString == "solid") {
+      transitionTime = 0;
+    }
+
+  }
+
+  return true;
+}
+
+
+/********************************** START CALLBACK*****************************************/
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  char message[length + 1];
+  for (int i = 0; i < length; i++) {
+    message[i] = (char)payload[i];
+  }
+  message[length] = '\0';
+  Serial.println(message);
+
+  if (!processJson(message)) {
+    return;
+  }
+
+  if (stateOn) {
+
+    realRed = map(red, 0, 255, 0, brightness);
+    realGreen = map(green, 0, 255, 0, brightness);
+    realBlue = map(blue, 0, 255, 0, brightness);
+  }
+  else {
+
+    realRed = 0;
+    realGreen = 0;
+    realBlue = 0;
+  }
+
+  Serial.println(effect);
+
+  startFade = true;
+  inFade = false; // Kill the current fade
+
+  sendState();
+}
